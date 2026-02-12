ea46fc9e060d02cb431ecccf10975faa3f759ec1
Moses <moseskarunia@gmail.com>
2026-02-12 16:07:32 +0700

    add qty safeguard

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let qty = 0;
 
 function substractQty() {
     qty = qty - 1; // OR qty -= 1
+
+    if (qty < 0) { // Same with ==> if (qty < 0) qty = 0;
+        qty = 0;
+    }
+
     document.getElementById('qty').innerHTML = qty;
 }
 
