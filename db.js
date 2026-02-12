7bf1b5a47d862b5d2be5a1233691922138d2cabc
Moses <moseskarunia@gmail.com>
2026-02-12 16:25:13 +0700

    simple menu db

---
+++ db.js
@@ -0,0 +1,55 @@
+const pizza = {
+    name: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    variant1: {
+        price: 15,
+        description: "Slice"
+    },
+    variant2: {
+        price: 60,
+        description: "Pan (6 Slices)"
+    },
+    photoUrl: "assets/pizza.jpg",
+};
+
+const wine = {
+    name: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    variant1: {
+        price: 100,
+        description: 'Glass'
+    },
+    variant2: {
+        price: 1500,
+        description: 'Bottle'
+    },
+    photoUrl: "assets/pizza.jpg"
+}
+
+const salad = {
+    name: 'Avocado Pesto Salad',
+    description: "A great meal becomes perfect when combined with a good wine",
+    variant1: {
+        price: 35,
+        description: ''
+    },
+    variant2: {
+        price: 55,
+        description: '+Grilled Salmon'
+    },
+    photoUrl: "assets/salad.jpg"
+}
+
+const coffee = {
+    name: 'Coffee',
+    description: "The best coffee you can have from the land of Indonesia",
+    variant1: {
+        price: 25,
+        description: 'Americano'
+    },
+    variant2: {
+        price: 30,
+        description: 'Latte'
+    },
+    photoUrl: "assets/coffee.jpg"
+}
