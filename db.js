e36933e9cc80e8bcb6d9988bfe6ff01e02b1f2f7
Moses <moseskarunia@gmail.com>
2026-02-26 16:12:59 +0700

    use loop to display menu card with length equals to menus

--- db.js
+++ db.js
@@ -1,47 +1,179 @@
-const menus = [
-    {
-        name: "Seasonal Wine",
-        description: "A great meal becomes perfect when combined with a good wine",
-        variants: [{
-            price: 100,
-            description: 'Glass'
-        }, {
-            price: 1500,
-            description: 'Bottle'
-        }],
-        photoUrl: "assets/wine.jpg"
+const menus = [{
+    name: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 100,
+        description: 'Glass'
     }, {
-        name: 'Coffee',
-        description: "The best coffee you can have from the land of Indonesia",
-        variants: [{
-            price: 25,
-            description: 'Americano'
-        }, {
-            price: 30,
-            description: 'Latte'
-        }],
-        photoUrl: "assets/coffee.jpg"
+        price: 1500,
+        description: 'Bottle'
+    }],
+    photoUrl: "assets/wine.jpg"
+}, {
+    name: 'Coffee',
+    description: "The best coffee you can have from the land of Indonesia",
+    variants: [{
+        price: 25,
+        description: 'Americano'
     }, {
-        name: "Triple Cheese Pizza",
-        description: "Indulge yourself in this simple yet delicious delicacy",
-        variants: [{
-            price: 15,
-            description: "Slice"
-        }, {
-            price: 60,
-            description: "Pan (6 Slices)"
-        }],
-        photoUrl: "assets/pizza.jpg",
+        price: 30,
+        description: 'Latte'
+    }],
+    photoUrl: "assets/coffee.jpg"
+}, {
+    name: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    variants: [{
+        price: 15,
+        description: "Slice"
     }, {
-        name: 'Avocado Pesto Salad',
-        description: "A great meal becomes perfect when combined with a good wine",
-        variants: [{
-            price: 35,
-            description: ''
-        }, {
-            price: 55,
-            description: '+Grilled Salmon'
-        }],
-        photoUrl: "assets/salad.jpg"
-    },
+        price: 60,
+        description: "Pan (6 Slices)"
+    }],
+    photoUrl: "assets/pizza.jpg",
+}, {
+    name: 'Avocado Pesto Salad',
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 35,
+        description: ''
+    }, {
+        price: 55,
+        description: '+Grilled Salmon'
+    }],
+    photoUrl: "assets/salad.jpg"
+}, {
+    name: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 100,
+        description: 'Glass'
+    }, {
+        price: 1500,
+        description: 'Bottle'
+    }],
+    photoUrl: "assets/wine.jpg"
+}, {
+    name: 'Coffee',
+    description: "The best coffee you can have from the land of Indonesia",
+    variants: [{
+        price: 25,
+        description: 'Americano'
+    }, {
+        price: 30,
+        description: 'Latte'
+    }],
+    photoUrl: "assets/coffee.jpg"
+}, {
+    name: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    variants: [{
+        price: 15,
+        description: "Slice"
+    }, {
+        price: 60,
+        description: "Pan (6 Slices)"
+    }],
+    photoUrl: "assets/pizza.jpg",
+}, {
+    name: 'Avocado Pesto Salad',
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 35,
+        description: ''
+    }, {
+        price: 55,
+        description: '+Grilled Salmon'
+    }],
+    photoUrl: "assets/salad.jpg"
+},
+{
+    name: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 100,
+        description: 'Glass'
+    }, {
+        price: 1500,
+        description: 'Bottle'
+    }],
+    photoUrl: "assets/wine.jpg"
+}, {
+    name: 'Coffee',
+    description: "The best coffee you can have from the land of Indonesia",
+    variants: [{
+        price: 25,
+        description: 'Americano'
+    }, {
+        price: 30,
+        description: 'Latte'
+    }],
+    photoUrl: "assets/coffee.jpg"
+}, {
+    name: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    variants: [{
+        price: 15,
+        description: "Slice"
+    }, {
+        price: 60,
+        description: "Pan (6 Slices)"
+    }],
+    photoUrl: "assets/pizza.jpg",
+}, {
+    name: 'Avocado Pesto Salad',
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 35,
+        description: ''
+    }, {
+        price: 55,
+        description: '+Grilled Salmon'
+    }],
+    photoUrl: "assets/salad.jpg"
+}, {
+    name: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 100,
+        description: 'Glass'
+    }, {
+        price: 1500,
+        description: 'Bottle'
+    }],
+    photoUrl: "assets/wine.jpg"
+}, {
+    name: 'Coffee',
+    description: "The best coffee you can have from the land of Indonesia",
+    variants: [{
+        price: 25,
+        description: 'Americano'
+    }, {
+        price: 30,
+        description: 'Latte'
+    }],
+    photoUrl: "assets/coffee.jpg"
+}, {
+    name: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    variants: [{
+        price: 15,
+        description: "Slice"
+    }, {
+        price: 60,
+        description: "Pan (6 Slices)"
+    }],
+    photoUrl: "assets/pizza.jpg",
+}, {
+    name: 'Avocado Pesto Salad',
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 35,
+        description: ''
+    }, {
+        price: 55,
+        description: '+Grilled Salmon'
+    }],
+    photoUrl: "assets/salad.jpg"
+},
 ];
--- index.js
+++ index.js
@@ -18,45 +18,50 @@ function addQty() {
 }
 
 function renderMenus() {
-    document.getElementById('menu-grid').innerHTML = `
-      <div class="menu-card">
-        <img src="${menus[0].photoUrl}" alt="${menus[0].name}" id="pic">
-        <h3 class="menu-name">${menus[0].name}</h3>
-        <p class="menu-description">${menus[0].description}</p>
-        <div class="menu-price-row">
-            <div class="price-description">${menus[0].variants[0].description}</div>
-            <div class="price-and-qty">
-                <h3 class="price">${menus[0].variants[0].price}</h3>
-                <button onclick="substractQty()">
-                    <span class="material-symbols-outlined">
-                        do_not_disturb_on
-                    </span>
-                </button>
-                <span class="qty" id="qty">0</span>
-                <button onclick="addQty()">
-                    <span class="material-symbols-outlined">
-                        add_circle
-                    </span>
-                </button>
+    let menuGrid = '';
+
+    for (let i = 0; i < menus.length; i++) {
+        menuGrid += `
+        <div class="menu-card">
+            <img src="${menus[0].photoUrl}" alt="${menus[0].name}" id="pic">
+            <h3 class="menu-name">${menus[0].name}</h3>
+            <p class="menu-description">${menus[0].description}</p>
+            <div class="menu-price-row">
+                <div class="price-description">${menus[0].variants[0].description}</div>
+                <div class="price-and-qty">
+                    <h3 class="price">${menus[0].variants[0].price}</h3>
+                    <button onclick="substractQty()">
+                        <span class="material-symbols-outlined">
+                            do_not_disturb_on
+                        </span>
+                    </button>
+                    <span class="qty" id="qty">0</span>
+                    <button onclick="addQty()">
+                        <span class="material-symbols-outlined">
+                            add_circle
+                        </span>
+                    </button>
+                </div>
             </div>
-        </div>
-        <div class="menu-price-row">
-            <p class="price-description">${menus[0].variants[1].description}</p>
-            <div class="price-and-qty">
-                <h3 class="price">${menus[0].variants[1].price}</h3>
-                <button onclick="document.getElementById('pic').src='assets/salad.jpg'">
-                    <span class="material-symbols-outlined">
-                        do_not_disturb_on
-                    </span>
-                </button>
-                <span class="qty">0</span>
-                <button onclick="document.getElementById('pic').src='assets/pizza.jpg'">
-                    <span class="material-symbols-outlined">
-                        add_circle
-                    </span>
-                </button>
+            <div class="menu-price-row">
+                <p class="price-description">${menus[0].variants[1].description}</p>
+                <div class="price-and-qty">
+                    <h3 class="price">${menus[0].variants[1].price}</h3>
+                    <button onclick="document.getElementById('pic').src='assets/salad.jpg'">
+                        <span class="material-symbols-outlined">
+                            do_not_disturb_on
+                        </span>
+                    </button>
+                    <span class="qty">0</span>
+                    <button onclick="document.getElementById('pic').src='assets/pizza.jpg'">
+                        <span class="material-symbols-outlined">
+                            add_circle
+                        </span>
+                    </button>
+                </div>
             </div>
-        </div>
-      </div>
-    `;
+        </div>`;
+    }
+
+    document.getElementById('menu-grid').innerHTML = menuGrid;
 }
